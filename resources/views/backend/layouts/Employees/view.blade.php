93a22ff74dd16da45d78b97266c843c8a9012cb8
RomanaIdressEkfa <ekfaeshita@gmail.com>
2023-11-19 01:49:14 +0600

    ui for view

--- resources/views/backend/layouts/Employees/view.blade.php
+++ resources/views/backend/layouts/Employees/view.blade.php
@@ -1,50 +1,53 @@
 @extends("backend.layouts.master")
 @section("content_page")
-    <div class="container p-3">
+    <div class="container border border-light shadow mt-2 rounded p-4">
 
-        <a href="{{ route("employee_details_index") }}">
-            <h4 class="text-primary mb-3"> <u>All Employee List</u></h4>
+        <a class="btn btn-primary btn-sm mt-2 mb-4 float-left bi bi-wallet2" href="{{ route("employee_details_index") }}">
+            All Employee List
         </a>
-        <a target="_blank" class="btn btn-primary btn-sm mt-2 mb-2 float-end" href="{{ route("employees_details_pdf", $employee->id) }}">Export PDF</a>
-        <div class="border border-1 p-3">
-            <table class="table table-bordered shadow-sm">
-                <h4 class="text-primary"> <u> <b>{{ $employee->name ?? "No Name Available" }}:</b> </u></h4>
-                <tr>
-                    <th>Name:</th>
-                    <td>{{ $employee->name ?? "No Name Available" }}</td>
-                </tr>
-                <tr>
-                    <th>Email:</th>
-                    <td>{{ $employee->email ?? "No Email Available" }}</td>
-                </tr>
-                <tr>
-                    <th>Address:</th>
-                    <td>{{ $employee->address ?? "No Address Available" }}</td>
-                </tr>
-                <tr>
-                    <th>Phone Number:</th>
-                    <td>{{ $employee->phone_number ?? "No Phone Number Available" }}</td>
-                </tr>
-                <tr>
-                    <th>Image:</th>
-                    <td>
-                        @if (isset($employee->image) && !empty($employee->image))
-                            <img style="width: 120px;" src="{{ asset("images/employees/" . $employee->image) }}" alt="Employee Image">
-                        @else
-                            No Picture Available
-                        @endif
-                    </td>
-                </tr>
+        <a target="_blank" class="btn btn-primary btn-sm mt-2 mb-2 ml-2 float-end bi bi-file-earmark-pdf-fill" href="{{ route("employees_details_pdf", $employee->id) }}">Export PDF</a>
+        <div>
+            <h6 class="text-gray-500 mb-5 mt-3" style='font-weight:bold'>VIEW EMPLOYEE</h6>
 
-                <tr>
-                    <th>NID No:</th>
-                    <td>{{ $employee->nid_no ?? "No NID No Available" }}</td>
-                </tr>
-                <tr>
-                    <th>Salary:</th>
-                    <td>{{ $employee->salary ?? "No Salary Available" }}</td>
-                </tr>
-            </table>
+        <h5 style="font-weight: bold">Name:</h5>
+        <p>{{ $employee->name ?? "No Name Available" }}</p>
+
+        <h5 style="font-weight: bold">Email:</h5>
+        <p>{{ $employee->email ?? "No Email Available" }}</p>
+
+        <h5 style="font-weight: bold">Address:</h5>
+        <p>{{ $employee->address ?? "No Address Available" }}</p>
+
+        <h5 style="font-weight: bold">Phone Number:</h5>
+        <p> {{ $employee->phone_number ?? "No Phone Number Available" }}</p>
+        
+        <h5 style="font-weight: bold">Department:</h5>
+        <p>{{ $employee->department ?? "No Department Available" }}</p>
+
+        <h5 style="font-weight: bold">Position:</h5>
+        <p>{{ $employee->position ?? "No Position Available" }}</p>
+
+        <h5 style="font-weight: bold">Date of Birth:</h5>
+        <p>{{ $employee->date_of_birth ?? "No Date of Birth Available" }}</p>
+
+        <h5 style="font-weight: bold">Hire Date:</h5>
+        <p>{{ $employee->hire_date ?? "No Hire Date Available" }}</p>
+
+        <h5 style="font-weight: bold">Image:</h5>
+        <p>
+             @if (isset($employee->image) && !empty($employee->image))
+            <img style="width: 120px;" src="{{ asset("images/employees/" . $employee->image) }}" alt="Employee Image">
+        @else
+            No Picture Available
+        @endif
+        </p>
+
+        <h5 style="font-weight: bold">NID No:</h5>
+        <p>{{ $employee->nid_no ?? "No NID No Available" }}</p>
+
+        <h5 style="font-weight: bold">Salary:</h5>
+        <p>{{ $employee->salary ?? "No Salary Available" }}</p>
+
         </div>
     </div>
 @endsection
--- resources/views/backend/layouts/PDF/employeePDF.blade.php
+++ resources/views/backend/layouts/PDF/employeePDF.blade.php
@@ -57,39 +57,46 @@
         <div class="container p-3">
             <div class="border p-4 shadow">
                 <h3 style="text-align: center; color:rgb(6, 53, 71)">{{ $employee->name }} Information</h3>
-                <table class="custom-table">
-                    <tr>
-                        <th>Name:</th>
-                        <td>{{ $employee->name }}</td>
-                    </tr>
-                    <tr>
-                        <th>Email:</th>
-                        <td>{{ $employee->email }}</td>
-                    </tr>
-                    <tr>
-                        <th>Address:</th>
-                        <td>{{ $employee->address }}</td>
-                    </tr>
-                    <tr>
-                        <th>Phone Number:</th>
-                        <td>{{ $employee->phone_number }}</td>
-                    </tr>
-                    <tr>
-                        <th>Image:</th>
-                        <td>
-                            <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path("images/employees/" . $employee->image))) }}" width="120">
 
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>NID No:</th>
-                        <td>{{ $employee->nid_no }}</td>
-                    </tr>
-                    <tr>
-                        <th>Salary:</th>
-                        <td>${{ $employee->salary }}</td>
-                    </tr>
-                </table>
+                            <h5 style="font-weight: bold">Name:</h5>
+                            <p>{{ isset($employee->name) ? $employee->name : "No Name Available" }}</p>
+
+                            <h5 style="font-weight: bold">Email:</h5>
+                            <p>{{ isset($employee->email) ? $employee->email : "No Email Available" }}</p>
+
+                            <h5 style="font-weight: bold">Image:</h5>
+                            <p>
+                            @if(isset($employee->image) && $employee->image)
+                                <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path("images/employees/" . $employee->image))) }}" width="120">
+                            @else
+                                No Image
+                            @endif
+                            </p>
+
+                <h5 style="font-weight: bold">Address:</h5>
+                <p>{{ $employee->address ?? "No Address Available" }}</p>
+
+                <h5 style="font-weight: bold">Phone Number:</h5>
+                <p> {{ $employee->phone_number ?? "No Phone Number Available" }}</p>
+
+                <h5 style="font-weight: bold">Department:</h5>
+                <p>{{ $employee->department ?? "No Department Available" }}</p>
+
+                <h5 style="font-weight: bold">Position:</h5>
+                <p>{{ $employee->position ?? "No Position Available" }}</p>
+
+                <h5 style="font-weight: bold">Date of Birth:</h5>
+                <p>{{ $employee->date_of_birth ?? "No Date of Birth Available" }}</p>
+
+                <h5 style="font-weight: bold">Hire Date:</h5>
+                <p>{{ $employee->hire_date ?? "No Hire Date Available" }}</p>
+
+                <h5 style="font-weight: bold">NID No:</h5>
+                <p>{{ $employee->nid_no ?? "No NID No Available" }}</p>
+
+                <h5 style="font-weight: bold">Salary:</h5>
+                <p>{{ $employee->salary ?? "No Salary Available" }}</p>
+
             </div>
         </div>
 
